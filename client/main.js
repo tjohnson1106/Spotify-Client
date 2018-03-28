9418eae4679e0213fa8f303f834ebd4523942a2a
Thomas_Johnson <tjohnson1106@gmail.com>
2018-03-28 15:16:23 -0400

    created http call back function

--- client/main.js
+++ client/main.js
@@ -1,2 +1,15 @@
 import { Template } from "meteor/templating";
 import { ReactiveVar } from "meteor/reactive-var";
+
+Template.search.events({
+  "keyup #searchArtists": function() {
+    let searchText = event.target.value;
+    Meteor.call("searchArtists", searchText, (err, artists) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(artists);
+      }
+    });
+  }
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.methods({
+  searchArtists(searchText) {
+    const result = HTTP.call(
+      "GET",
+      "https://api.spotify.com/v1/search?query='+searchText+' &offset=0&limit=20&type=artist&market=US"
+    );
+    return result;
+  }
 });
